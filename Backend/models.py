cbf5832a42f6859a3a7d8ae4729e13aecafa905c
Arya Aditanshu Behera <AryaAditanshu.Behera@ust.trumio.ai>
2026-01-17 10:44:42 +0530

    Made changes in models

--- Backend/models.py
+++ Backend/models.py
@@ -6,14 +6,17 @@ class Course(Base):
     __tablename__ = "courses"
 
     course_name = Column(String, primary_key=True, index=True)
-    instructor = Column(String, index=True)
+    instructor_id = Column(Integer, index=True)
+    instructor_name = Column(String, index=True)
     duration = Column(Integer)
 
     enrollments = relationship("Enrollment", back_populates="course", foreign_keys="Enrollment.course_id")
 
 class Enrollment(Base):
     __tablename__ = "enrollments"
-    student_name = Column(String, primary_key=True, index=True)
+    
+    student_id = Column(Integer, primary_key=True, index=True)
+    student_name = Column(String, index=True)
     course_id = Column(String, ForeignKey("courses.course_name"), primary_key=True)
     status = Column(String, default="Enrolled")
 
